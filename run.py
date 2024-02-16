d32b1f317d72ccaa94f1c57f8904495d32c63b6d
Sreeshma Tharammal <sreeshmajeenus@gmail.com>
2024-02-16 18:49:35 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -55,6 +55,8 @@ def validate_data(values):
 
 def update_worksheet(data, worksheet):
     """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
     """
     print(f"Updating {worksheet} worksheet.....\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
@@ -79,6 +81,21 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
 
     return surplus_data
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 def main():
     """
